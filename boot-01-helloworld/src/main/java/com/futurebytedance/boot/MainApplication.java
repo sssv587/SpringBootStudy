5ef8cfeddef10e1094a8441824817188c4c8baec
sssv587 <1092663295@qq.com>
2022-09-20 00:35:47 +0800

    SpringBoot-自动配置原理

--- boot-01-helloworld/src/main/java/com/futurebytedance/boot/MainApplication.java
+++ boot-01-helloworld/src/main/java/com/futurebytedance/boot/MainApplication.java
@@ -1,7 +1,11 @@
 package com.futurebytedance.boot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author yuhang.sun
@@ -10,9 +14,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Description
  */
 //主程序类:@SpringBootApplication 这是一个SpringBoot应用
-@SpringBootApplication
+//@SpringBootApplication(scanBasePackages = "com.futurebytedance")
+@SpringBootConfiguration
+@EnableAutoConfiguration
+@ComponentScan("com.futurebytedance")
 public class MainApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class, args);
+        //1、返回我们的IOC容器
+        ConfigurableApplicationContext run = SpringApplication.run(MainApplication.class, args);
+        //2、查看容器里面的组件
+        String[] names = run.getBeanDefinitionNames();
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
--- boot-01-helloworld/src/main/java/com/futurebytedance/boot/controller/HelloController.java
+++ boot-01-helloworld/src/main/java/com/futurebytedance/boot/controller/HelloController.java
@@ -15,6 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String handle01() {
-        return "Hello, Spring Boot 2!";
+        return "Hello, Spring Boot 2! 你好!";
     }
 }
