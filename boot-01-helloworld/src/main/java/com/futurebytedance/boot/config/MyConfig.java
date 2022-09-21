bdafc87cf783e1670317499e7a1fe36dbd079029
sssv587 <1092663295@qq.com>
2022-09-22 00:03:25 +0800

    SpringBoot-@ImportResource

--- boot-01-helloworld/src/main/java/com/futurebytedance/boot/config/MyConfig.java
+++ boot-01-helloworld/src/main/java/com/futurebytedance/boot/config/MyConfig.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * @author yuhang.sun
@@ -22,11 +23,13 @@ import org.springframework.context.annotation.Import;
  * 组件依赖必须使用Full模式默认。其他默认是否Lite模式
  * 4、@Import({User.class, DBHelper.class})
  *     给容器中自动创建出这两个类型的组件,默认组件的名字就是全类名
+ * 5、@ImportResource("classpath:beans.xml")导入Spring的配置文件,使其生效
  */
 @Import({User.class, DBHelper.class})
 @Configuration(proxyBeanMethods = true) //告诉SpringBoot这时一个配置类 == 配置文件
 //@ConditionalOnBean(name = "tom")
 @ConditionalOnMissingBean(name = "tom")
+@ImportResource("classpath:beans.xml")
 public class MyConfig {
     /**
      * 外部无论对配置类中的这个组件注册方法调用多少次获取的都是之前注册容器中的单实例对象
