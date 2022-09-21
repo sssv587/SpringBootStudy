95952bfaa10452f46e8c50b1cfb5f232e6502f8e
sssv587 <1092663295@qq.com>
2022-09-21 23:50:47 +0800

    SpringBoot-@Import导入组件

--- boot-01-helloworld/src/main/java/com/futurebytedance/boot/MainApplication.java
+++ boot-01-helloworld/src/main/java/com/futurebytedance/boot/MainApplication.java
@@ -1,5 +1,6 @@
 package com.futurebytedance.boot;
 
+import ch.qos.logback.core.db.DBHelper;
 import com.futurebytedance.boot.bean.Pet;
 import com.futurebytedance.boot.bean.User;
 import com.futurebytedance.boot.config.MyConfig;
@@ -10,17 +11,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
+import java.util.Arrays;
+
 /**
  * @author yuhang.sun
  * @version 1.0
  * @date 2022/9/19 - 23:43
  * @Description
  */
+//主程序类:主配置类
 //主程序类:@SpringBootApplication 这是一个SpringBoot应用
-//@SpringBootApplication(scanBasePackages = "com.futurebytedance")
-@SpringBootConfiguration
-@EnableAutoConfiguration
-@ComponentScan("com.futurebytedance")
+@SpringBootApplication(scanBasePackages = "com.futurebytedance")
+//@SpringBootConfiguration
+//@EnableAutoConfiguration
+//@ComponentScan("com.futurebytedance")
 public class MainApplication {
     public static void main(String[] args) {
         //1、返回我们的IOC容器
@@ -49,5 +53,12 @@ public class MainApplication {
         User user01 = run.getBean("user01", User.class);
         Pet tom = run.getBean("tom", Pet.class);
         System.out.println("用户的宠物:" + (user01.getPet() == tom));
+
+        //5、获取组件
+        String[] beanNamesForType = run.getBeanNamesForType(User.class);
+        System.out.println(Arrays.toString(beanNamesForType));
+
+        DBHelper bean1 = run.getBean(DBHelper.class);
+        System.out.println(bean1);
     }
 }
--- boot-01-helloworld/src/main/java/com/futurebytedance/boot/config/MyConfig.java
+++ boot-01-helloworld/src/main/java/com/futurebytedance/boot/config/MyConfig.java
@@ -1,22 +1,27 @@
 package com.futurebytedance.boot.config;
 
+import ch.qos.logback.core.db.DBHelper;
 import com.futurebytedance.boot.bean.Pet;
 import com.futurebytedance.boot.bean.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
  * @author yuhang.sun
  * @version 1.0
  * @date 2022/9/21 - 23:22
  * @Description
+ * 1.配置类里面使用@Bean标注在方法上给容器注册组件,默认也是单实例的
+ * 2、配置类本身也是组件
+ * 3、proxyBeanMethods:代理bean方法
+ *      Full(proxyBeanMethods = true)【保证每个@Bean方法被调用多少次返回的组件都是单实例的】
+ *      Lite(proxyBeanMethods = false)【每个@Bean方法被调用多少次返回的组件都是新创建的】
+ * 组件依赖必须使用Full模式默认。其他默认是否Lite模式
+ * 4、@Import({User.class, DBHelper.class})
+ *     给容器中自动创建出这两个类型的组件,默认组件的名字就是全类名
  */
-//1.配置类里面使用@Bean标注在方法上给容器注册组件,默认也是单实例的
-//2、配置类本身也是组件
-//3、proxyBeanMethods:代理bean方法
-// Full(proxyBeanMethods = true)
-// Lite(proxyBeanMethods = false)
-// 组建依赖
+@Import({User.class, DBHelper.class})
 @Configuration(proxyBeanMethods = true) //告诉SpringBoot这时一个配置类 == 配置文件
 public class MyConfig {
     /**
