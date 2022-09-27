4dae9c3e113692710100bef00af991f836bf0d2c
sssv587 <1092663295@qq.com>
2022-09-28 00:19:36 +0800

    SpringBoot源码分析-Model、Map原理

--- boot-05-web-01/src/main/java/com/futurebytedance/boot/controller/RequestController.java
+++ boot-05-web-01/src/main/java/com/futurebytedance/boot/controller/RequestController.java
@@ -1,12 +1,16 @@
 package com.futurebytedance.boot.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
-
 /**
  * @author yuhang.sun
  * @version 1.0
@@ -22,17 +26,39 @@ public class RequestController {
         return "forward:/success"; //转发到 /success请求
     }
 
+    @GetMapping("/params")
+    public String testParam(Map<String,Object> map,
+                            Model model,
+                            HttpServletRequest request,
+                            HttpServletResponse response){
+        map.put("hello","world666");
+        model.addAttribute("world","hello666");
+        request.setAttribute("message","HelloWorld");
+
+        Cookie cookie = new Cookie("c1","v1");
+        response.addCookie(cookie);
+        return "forward:/success";
+    }
+
     @ResponseBody
     @GetMapping("/success")
-    public Map success(@RequestAttribute("msg") String msg,
-                       @RequestAttribute("code") Integer code,
-                       HttpServletRequest request) {
-        String msg1 = request.getAttribute("msg").toString();
+    public Map success(@RequestAttribute(value = "msg",required = false) String msg,
+                       @RequestAttribute(value = "code",required = false)Integer code,
+                       HttpServletRequest request){
+        Object msg1 = request.getAttribute("msg");
 
-        Map<String, String> map = new HashMap<>();
+        Map<String,Object> map = new HashMap<>();
+        Object hello = request.getAttribute("hello");
+        Object world = request.getAttribute("world");
+        Object message = request.getAttribute("message");
 
-        map.put("requestMethod_msg", msg1);
-        map.put("annotation_msg", msg);
+        map.put("reqMethod_msg",msg1);
+        map.put("annotation_msg",msg);
+        map.put("hello",hello);
+        map.put("world",world);
+        map.put("message",message);
+
         return map;
+
     }
 }
