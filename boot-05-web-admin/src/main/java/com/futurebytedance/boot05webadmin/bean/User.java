87cf74ba07758004d149b3130699bb59ff6fc7b7
sssv587 <1092663295@qq.com>
2022-10-07 21:21:00 +0800

    后台管理系统基本功能

---
+++ boot-05-web-admin/src/main/java/com/futurebytedance/boot05webadmin/bean/User.java
@@ -0,0 +1,15 @@
+package com.futurebytedance.boot05webadmin.bean;
+
+import lombok.Data;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2022/10/7 - 21:07
+ * @Description
+ */
+@Data
+public class User {
+    private String userName;
+    private String password;
+}
