bb3db8f3a9f998db199d82b3c09c0b0183663938
sssv587 <1092663295@qq.com>
2022-10-07 20:56:37 +0800

    init

---
+++ boot-05-web-admin/src/test/java/com/futurebytedance/boot05webadmin/Boot05WebAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futurebytedance.boot05webadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot05WebAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
