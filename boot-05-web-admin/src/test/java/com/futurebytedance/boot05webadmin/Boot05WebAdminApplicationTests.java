340d3788a49b4505b75bb68e2ae746c4d66c2a79
sssv587 <1092663295@qq.com>
2022-10-12 23:40:11 +0800

    数据访问-数据库场景的自动配置分析与整合测试

--- boot-05-web-admin/src/test/java/com/futurebytedance/boot05webadmin/Boot05WebAdminApplicationTests.java
+++ boot-05-web-admin/src/test/java/com/futurebytedance/boot05webadmin/Boot05WebAdminApplicationTests.java
@@ -1,13 +1,22 @@
 package com.futurebytedance.boot05webadmin;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+@Slf4j
 @SpringBootTest
 class Boot05WebAdminApplicationTests {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @Test
     void contextLoads() {
+//        jdbcTemplate.queryForObject("select * from customers");
+        Long aLong = jdbcTemplate.queryForObject("select count(1) from customers", Long.class);
+        log.info("记录总数:{}", aLong);
     }
 
 }
