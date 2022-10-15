f4e8cc8e809479b53ea71c64c02d783b4198262b
sssv587 <1092663295@qq.com>
2022-10-15 21:09:22 +0800

    数据访问-删除用户功能

--- boot-05-web-admin/src/main/java/com/futurebytedance/boot05webadmin/controller/TableController.java
+++ boot-05-web-admin/src/main/java/com/futurebytedance/boot05webadmin/controller/TableController.java
@@ -9,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Arrays;
 import java.util.List;
@@ -37,6 +39,16 @@ public class TableController {
         return "table/basic_table";
     }
 
+    @GetMapping("/user/delete/{id}")
+    public String deleteUser(@PathVariable("id") Long id,
+                             @RequestParam(value = "pn", defaultValue = "1") Integer pn,
+                             RedirectAttributes ra) {
+        userService.removeById(id);
+
+        ra.addAttribute("pn", pn);
+        return "redirect:/dynamic_table";
+    }
+
     @GetMapping("/dynamic_table")
     public String dynamic_table(@RequestParam(value = "pn", defaultValue = "1") Integer pn, Model model) { //RedirectAttributes
         //表格内容的遍历
@@ -57,12 +69,12 @@ public class TableController {
 //        model.addAttribute("users", list);
 
         //分页查询数据
-        Page<User> userPage = new Page<>(pn, 2);
+        Page<User> page = new Page<>(pn, 2);
 
         //分页查询的结果
-        Page<User> page = userService.page(userPage, null);
+        Page<User> userPage = userService.page(page, null);
 
-        model.addAttribute("page", page);
+        model.addAttribute("users", userPage);
 
         return "table/dynamic_table";
     }
