fb86600f2746dd488fe91a4c8b8cd02f9267f75d
sssv587 <1092663295@qq.com>
2022-10-15 21:02:52 +0800

    数据访问-分页展示

--- boot-05-web-admin/src/main/java/com/futurebytedance/boot05webadmin/controller/TableController.java
+++ boot-05-web-admin/src/main/java/com/futurebytedance/boot05webadmin/controller/TableController.java
@@ -1,7 +1,11 @@
 package com.futurebytedance.boot05webadmin.controller;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.futurebytedance.boot05webadmin.bean.User;
 import com.futurebytedance.boot05webadmin.exception.UserTooManyException;
+import com.futurebytedance.boot05webadmin.service.UserService;
+import com.futurebytedance.boot05webadmin.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +22,9 @@ import java.util.List;
  */
 @Controller
 public class TableController {
+    @Autowired
+    UserService userService;
+
     /**
      * 不带请求参数或者参数类型不读 400：Bad Request 一般都是浏览器的参数没有传递正确
      *
@@ -31,7 +38,7 @@ public class TableController {
     }
 
     @GetMapping("/dynamic_table")
-    public String dynamic_table(Model model) { //RedirectAttributes
+    public String dynamic_table(@RequestParam(value = "pn", defaultValue = "1") Integer pn, Model model) { //RedirectAttributes
         //表格内容的遍历
 //        List<User> users = Arrays.asList(new User("zhangsan", "123456"),
 //                new User("lisi", "123444"),
@@ -44,7 +51,18 @@ public class TableController {
 //            throw new UserTooManyException();
 //        }
 
-        //从数据库中
+        //从数据库中查出user表中的用户进行展示
+        List<User> list = userService.list();
+
+//        model.addAttribute("users", list);
+
+        //分页查询数据
+        Page<User> userPage = new Page<>(pn, 2);
+
+        //分页查询的结果
+        Page<User> page = userService.page(userPage, null);
+
+        model.addAttribute("page", page);
 
         return "table/dynamic_table";
     }
