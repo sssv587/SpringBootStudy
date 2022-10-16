4d08024cade3b791b6e8315aea92fa03c1349581
sssv587 <1092663295@qq.com>
2022-10-17 00:08:00 +0800

    init

---
+++ boot-05-adminserver/src/main/java/com/futurebytedance/boot05adminserver/Boot05AdminserverApplication.java
@@ -0,0 +1,13 @@
+package com.futurebytedance.boot05adminserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot05AdminserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot05AdminserverApplication.class, args);
+    }
+
+}
---
+++ boot-05-adminserver/src/test/java/com/futurebytedance/boot05adminserver/Boot05AdminserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futurebytedance.boot05adminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot05AdminserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
