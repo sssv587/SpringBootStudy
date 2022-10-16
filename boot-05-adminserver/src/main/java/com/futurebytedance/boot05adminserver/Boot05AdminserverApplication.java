47626c568774434ef9b25beda216af09c8eb5a27
sssv587 <1092663295@qq.com>
2022-10-17 00:37:29 +0800

    指标监控-Boot Admin Server

--- boot-05-adminserver/src/main/java/com/futurebytedance/boot05adminserver/Boot05AdminserverApplication.java
+++ boot-05-adminserver/src/main/java/com/futurebytedance/boot05adminserver/Boot05AdminserverApplication.java
@@ -1,8 +1,11 @@
 package com.futurebytedance.boot05adminserver;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//https://github.com/codecentric/spring-boot-admin
+@EnableAdminServer
 @SpringBootApplication
 public class Boot05AdminserverApplication {
 
