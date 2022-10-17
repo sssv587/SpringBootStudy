a92e1f24584c92164f869dae310b578e163f3195
sssv587 <1092663295@qq.com>
2022-10-18 00:34:28 +0800

    init

---
+++ boot-09-customer-starter/src/main/java/com/futurebytedance/boot09customerstarter/Boot09CustomerStarterApplication.java
@@ -0,0 +1,13 @@
+package com.futurebytedance.boot09customerstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot09CustomerStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot09CustomerStarterApplication.class, args);
+    }
+
+}
---
+++ boot-09-customer-starter/src/test/java/com/futurebytedance/boot09customerstarter/Boot09CustomerStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futurebytedance.boot09customerstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot09CustomerStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
